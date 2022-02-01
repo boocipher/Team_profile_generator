cd7695162d40767667898f3726f2aec1e205de6f
boocipher <boocipher@gmail.com>
2022-02-01 12:01:59 -0600

    adds Mngr to teamMembers

--- index.js
+++ index.js
@@ -7,3 +7,38 @@ const fs = require('fs');
 const path = require('path');
 
 const htmlTemplate = require('./utils/generateHTML');
+
+// Array to push all new team members
+const teamMembers = [];
+
+function ManagerQuestions() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the manager's name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the manager's ID?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the manager's email?"
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "What is the manager's office number?"
+        }
+    ])
+    .then(res => {
+        const manager = new Manager(res.name, res.id, res.email, res.officeNumber);
+        
+        teamMembers.push(manager);
+        cycleQuestion();
+        // console.log(teamMembers);
+    })
+}
