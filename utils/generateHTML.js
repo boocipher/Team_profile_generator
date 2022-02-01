4aca97c29994a9379d915ad54076bd72389c28cf
boocipher <boocipher@gmail.com>
2022-01-31 23:41:46 -0600

    dir structure and html

---
+++ utils/generateHTML.js
@@ -0,0 +1,29 @@
+// const generateCard = require('./generateCard')
+
+
+const htmlTemplate = (teamMembers) => {
+    return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
+    rel="stylesheet" 
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" 
+    crossorigin="anonymous">
+    <link rel="stylesheet" 
+    href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" 
+    integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" 
+    crossorigin="anonymous"/>
+    <title>Team Profile Generator</title>
+</head>
+<body>
+    <h1 class='text-center p-5 bg-primary text-white'>My Team</h1>
+    <div class='d-flex container justify-content-center gap-5 flex-wrap mt-5'>${generateTeam(teamMembers)}</div>
+</body>
+</html>`
+}
+
+module.exports = htmlTemplate 
