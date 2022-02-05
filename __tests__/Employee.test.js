24a61c4f9d84da6a508cff8934120c24c70ad6f8
boocipher <boocipher@gmail.com>
2022-02-04 21:07:25 -0600

    adds more tests to Employee

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -6,14 +6,50 @@ const Employee = require('../lib/Employee');
 //     })
 // })
 
-test("Can set name via constructor argument", ()=> {
+test("Can set name via constructor method", ()=> {
     const name = "Aaron";
     const emp = new Employee(name);
     expect(emp.name).toBe(name)
 })
 
-test("getName returns employees name", ()=> {
+test("getName returns employee's name", ()=> {
     const name = "Aaron";
     const emp = new Employee(name);
     expect(emp.getName()).toBe(name);
+})
+
+test("Can set id via constructor method", ()=> {
+    const id = "123";
+    const emp = new Employee(undefined, id, undefined);
+    expect(emp.id).toBe(id)
+})
+
+test("User enters ID as a numeric string", ()=> {
+    const id = 123;
+    const emp = new Employee(undefined, id, undefined);
+    expect(typeof emp.id).toBe('number')
+})
+
+test("getId returns employee's id", ()=> {
+    const id = "123";
+    const emp = new Employee(undefined, "123", undefined);
+    expect(emp.getId()).toBe(id)
+})
+
+test("Can set email via constructor method", ()=> {
+    const email = "email@myco.com";
+    const emp = new Employee(undefined, undefined, "email@myco.com");
+    expect(emp.email).toBe(email)
+})
+
+test("getEmail returns email address", ()=> {
+    const email = "email@myco.com";
+    const emp = new Employee(undefined, undefined, email);
+    expect(emp.getEmail()).toBe(email)
+})
+
+test("getRole returns role", ()=> {
+    const role = "Employee";
+    const emp = new Employee(undefined, undefined, undefined);
+    expect(emp.getRole()).toBe(role)
 })
